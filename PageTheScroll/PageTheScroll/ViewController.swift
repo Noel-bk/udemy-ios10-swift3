072af3944787df635a0ef3d3395924e698cce5ea
Noel.b <jaeyeong.b@gmail.com>
2017-01-25 22:25:37 +0900

    Add UIScrollView

---
+++ PageTheScroll/PageTheScroll/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  PageTheScroll
+//
+//  Created by Noel Nolan Masters on 1/22/17.
+//  Copyright © 2017 Noel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images = [UIImageView]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        var contentWidth: CGFloat = 0.0
+        
+        let scrollWidth = scrollView.frame.size.width
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - (150/2), y: (scrollView.frame.size.height/2) - (150/2), width: 150, height: 150)
+        }
+        
+//        scrollView.backgroundColor = UIColor.darkGray
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+    }
+    
+}
+
