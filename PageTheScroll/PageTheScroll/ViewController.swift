c0ce7cf46bbc053b7ee1797e9300e2e741d4efc4
Noel.b <jaeyeong.b@gmail.com>
2017-02-05 23:43:23 +0900

    Clean up

--- PageTheScroll/PageTheScroll/ViewController.swift
+++ PageTheScroll/PageTheScroll/ViewController.swift
@@ -39,7 +39,7 @@ class ViewController: UIViewController {
         }
         
 //        scrollView.backgroundColor = UIColor.sddarkGray
-        scrollView.clipsToBounds = falsesdf
+        scrollView.clipsToBounds = false
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
     
