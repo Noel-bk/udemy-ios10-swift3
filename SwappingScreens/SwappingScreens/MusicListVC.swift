8f428ff6412fb32eec967932a4b84d35dcca057c
Noel.b <jaeyeong.b@gmail.com>
2017-02-07 00:31:03 +0900

    Add segue via code

--- SwappingScreens/SwappingScreens/MusicListVC.swift
+++ SwappingScreens/SwappingScreens/MusicListVC.swift
@@ -21,14 +21,17 @@ class MusicListVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func load3rdScreenPressed(_ sender: Any) {
+        let songTitle = "Against all odds"
+        performSegue(withIdentifier: "PlaySongVC", sender: songTitle)
     }
-    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? PlaySongVC {
+            if let song = sender as? String {
+                destination.selectedSong = song
+            }
+        }
+    }
 
 }
