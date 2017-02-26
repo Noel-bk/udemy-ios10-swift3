9bd91f37ab4df7497d8a30a5cdfb909edd6b1ef7
Noel.b <jaeyeong.b@gmail.com>
2017-02-26 23:43:02 +0900

    Init PartyRockApp

---
+++ PartyRockApp/PartyRockApp/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Noel Nolan Masters on 2/26/17.
+//  Copyright © 2017 Noel. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
